49cf342958253d07167aa7911f023ce9843c375e
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2021-06-02 11:09:21 -0300

    Estrutura Condicional

---
+++ src/main/java/br/com/gabriel/aula/conceitos/estrutura/condicional/Exercicio8.java
@@ -0,0 +1,18 @@
+package br.com.gabriel.aula.conceitos.estrutura.condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio8 {
+
+  public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+
+
+    Scanner sc = new Scanner(System.in);
+
+    sc.close();
+  }
+
+}
