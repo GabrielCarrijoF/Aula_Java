37940af16367a8e02a803b18f511fdc1e06eb3ea
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2021-06-04 11:45:54 -0300

    Estrutura Repetitiva, uso do While

--- src/main/java/br/com/gabriel/aula/conceitos/estrutura/repetitiva/Senhas.java
+++ src/main/java/br/com/gabriel/aula/conceitos/estrutura/repetitiva/Senhas.java
@@ -7,7 +7,7 @@ public class Senhas {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
-    int senha = 00;
+    int senha = 0;
 
     while (senha != 2002) {
       System.out.println("Digite sua senha??");
@@ -17,7 +17,6 @@ public class Senhas {
     System.out.println("Senha correta");
 
     sc.close();
-
   }
 
 }
