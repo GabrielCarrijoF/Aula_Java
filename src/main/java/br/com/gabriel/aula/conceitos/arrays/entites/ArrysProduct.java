fccd944a744cfd173eaa9b4de5e626c4af320ea4
Gabriel Ferreira Carrijo <gabriel.carrijo@luizalabs.com>
2021-06-25 09:00:32 -0300

    Exercicio dois de Vetores

---
+++ src/main/java/br/com/gabriel/aula/conceitos/arrays/entites/ArrysProduct.java
@@ -0,0 +1,29 @@
+package br.com.gabriel.aula.conceitos.arrays.entites;
+
+public class ArrysProduct {
+
+  private String name;
+  private double price;
+
+  public ArrysProduct(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+}
